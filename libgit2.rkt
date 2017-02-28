19f9e22df6bf1ff6647f9d6b1090551ed8936f37
Bradley Busching <bradley.busching@gmail.com>
2017-02-27 23:59:16 -0800

    Create include folder
    This clears up the repo root making it easier to browse.

--- libgit2.rkt
+++ libgit2.rkt
@@ -1,112 +1,112 @@
 #lang racket
 
-(require "annotated_commit.rkt"
-         "blame.rkt"
-         "blob.rkt"
-         "branch.rkt"
-         "buffer.rkt"
-         "checkout.rkt"
-         "cherrypick.rkt"
-         "clone.rkt"
-         "commit.rkt"
-         "config.rkt"
-         "cred_helpers.rkt"
-         "describe.rkt"
-         "diff.rkt"
-         "errors.rkt"
-         "filter.rkt"
-         "global.rkt"
-         "graph.rkt"
-         "ignore.rkt"
-         "index.rkt"
-         "indexer.rkt"
-         "merge.rkt"
-         "message.rkt"
-         "net.rkt"
-         "notes.rkt"
-         "object.rkt"
-         "odb.rkt"
-         "odb_backend.rkt"
-         "oid.rkt"
-         "oidarray.rkt"
-         "pack.rkt"
-         "patch.rkt"
-         "pathspec.rkt"
-         "proxy.rkt"
-         "rebase.rkt"
-         "refdb.rkt"
-         "reflog.rkt"
-         "refs.rkt"
-         "refspec.rkt"
-         "remote.rkt"
-         "repository.rkt"
-         "reset.rkt"
-         "revert.rkt"
-         "revparse.rkt"
-         "revwalk.rkt"
-         "signature.rkt"
-         "status.rkt"
-         "strarray.rkt"
-         "submodule.rkt"
-         "tag.rkt"
-         "trace.rkt"
-         "transaction.rkt"
-         "transport.rkt"
-         "tree.rkt"
-	 "types.rkt")
+(require "include/annotated_commit.rkt"
+         "include/blame.rkt"
+         "include/blob.rkt"
+         "include/branch.rkt"
+         "include/buffer.rkt"
+         "include/checkout.rkt"
+         "include/cherrypick.rkt"
+         "include/clone.rkt"
+         "include/commit.rkt"
+         "include/config.rkt"
+         "include/cred_helpers.rkt"
+         "include/describe.rkt"
+         "include/diff.rkt"
+         "include/errors.rkt"
+         "include/filter.rkt"
+         "include/global.rkt"
+         "include/graph.rkt"
+         "include/ignore.rkt"
+         "include/index.rkt"
+         "include/indexer.rkt"
+         "include/merge.rkt"
+         "include/message.rkt"
+         "include/net.rkt"
+         "include/notes.rkt"
+         "include/object.rkt"
+         "include/odb.rkt"
+         "include/odb_backend.rkt"
+         "include/oid.rkt"
+         "include/oidarray.rkt"
+         "include/pack.rkt"
+         "include/patch.rkt"
+         "include/pathspec.rkt"
+         "include/proxy.rkt"
+         "include/rebase.rkt"
+         "include/refdb.rkt"
+         "include/reflog.rkt"
+         "include/refs.rkt"
+         "include/refspec.rkt"
+         "include/remote.rkt"
+         "include/repository.rkt"
+         "include/reset.rkt"
+         "include/revert.rkt"
+         "include/revparse.rkt"
+         "include/revwalk.rkt"
+         "include/signature.rkt"
+         "include/status.rkt"
+         "include/strarray.rkt"
+         "include/submodule.rkt"
+         "include/tag.rkt"
+         "include/trace.rkt"
+         "include/transaction.rkt"
+         "include/transport.rkt"
+         "include/tree.rkt"
+	 "include/types.rkt")
 
-(provide (all-from-out "annotated_commit.rkt")
-         (all-from-out "blame.rkt")
-         (all-from-out "blob.rkt")
-         (all-from-out "branch.rkt")
-         (all-from-out "buffer.rkt")
-         (all-from-out "checkout.rkt")
-         (all-from-out "cherrypick.rkt")
-         (all-from-out "clone.rkt")
-         (all-from-out "commit.rkt")
-         (all-from-out "config.rkt")
-         (all-from-out "cred_helpers.rkt")
-         (all-from-out "describe.rkt")
-         (all-from-out "diff.rkt")
-         (all-from-out "errors.rkt")
-         (all-from-out "filter.rkt")
-         (all-from-out "global.rkt")
-         (all-from-out "graph.rkt")
-         (all-from-out "ignore.rkt")
-         (all-from-out "index.rkt")
-         (all-from-out "indexer.rkt")
-         (all-from-out "merge.rkt")
-         (all-from-out "message.rkt")
-         (all-from-out "net.rkt")
-         (all-from-out "notes.rkt")
-         (all-from-out "object.rkt")
-         (all-from-out "odb.rkt")
-         (all-from-out "odb_backend.rkt")
-         (all-from-out "oid.rkt")
-         (all-from-out "oidarray.rkt")
-         (all-from-out "pack.rkt")
-         (all-from-out "patch.rkt")
-         (all-from-out "pathspec.rkt")
-         (all-from-out "proxy.rkt")
-         (all-from-out "rebase.rkt")
-         (all-from-out "refdb.rkt")
-         (all-from-out "reflog.rkt")
-         (all-from-out "refs.rkt")
-         (all-from-out "refspec.rkt")
-         (all-from-out "remote.rkt")
-         (all-from-out "repository.rkt")
-         (all-from-out "reset.rkt")
-         (all-from-out "revert.rkt")
-         (all-from-out "revparse.rkt")
-         (all-from-out "revwalk.rkt")
-         (all-from-out "signature.rkt")
-         (all-from-out "status.rkt")
-         (all-from-out "strarray.rkt")
-         (all-from-out "submodule.rkt")
-         (all-from-out "tag.rkt")
-         (all-from-out "trace.rkt")
-         (all-from-out "transaction.rkt")
-         (all-from-out "transport.rkt")
-         (all-from-out "tree.rkt")
-         (all-from-out "types.rkt"))
+(provide (all-from-out "include/annotated_commit.rkt")
+         (all-from-out "include/blame.rkt")
+         (all-from-out "include/blob.rkt")
+         (all-from-out "include/branch.rkt")
+         (all-from-out "include/buffer.rkt")
+         (all-from-out "include/checkout.rkt")
+         (all-from-out "include/cherrypick.rkt")
+         (all-from-out "include/clone.rkt")
+         (all-from-out "include/commit.rkt")
+         (all-from-out "include/config.rkt")
+         (all-from-out "include/cred_helpers.rkt")
+         (all-from-out "include/describe.rkt")
+         (all-from-out "include/diff.rkt")
+         (all-from-out "include/errors.rkt")
+         (all-from-out "include/filter.rkt")
+         (all-from-out "include/global.rkt")
+         (all-from-out "include/graph.rkt")
+         (all-from-out "include/ignore.rkt")
+         (all-from-out "include/index.rkt")
+         (all-from-out "include/indexer.rkt")
+         (all-from-out "include/merge.rkt")
+         (all-from-out "include/message.rkt")
+         (all-from-out "include/net.rkt")
+         (all-from-out "include/notes.rkt")
+         (all-from-out "include/object.rkt")
+         (all-from-out "include/odb.rkt")
+         (all-from-out "include/odb_backend.rkt")
+         (all-from-out "include/oid.rkt")
+         (all-from-out "include/oidarray.rkt")
+         (all-from-out "include/pack.rkt")
+         (all-from-out "include/patch.rkt")
+         (all-from-out "include/pathspec.rkt")
+         (all-from-out "include/proxy.rkt")
+         (all-from-out "include/rebase.rkt")
+         (all-from-out "include/refdb.rkt")
+         (all-from-out "include/reflog.rkt")
+         (all-from-out "include/refs.rkt")
+         (all-from-out "include/refspec.rkt")
+         (all-from-out "include/remote.rkt")
+         (all-from-out "include/repository.rkt")
+         (all-from-out "include/reset.rkt")
+         (all-from-out "include/revert.rkt")
+         (all-from-out "include/revparse.rkt")
+         (all-from-out "include/revwalk.rkt")
+         (all-from-out "include/signature.rkt")
+         (all-from-out "include/status.rkt")
+         (all-from-out "include/strarray.rkt")
+         (all-from-out "include/submodule.rkt")
+         (all-from-out "include/tag.rkt")
+         (all-from-out "include/trace.rkt")
+         (all-from-out "include/transaction.rkt")
+         (all-from-out "include/transport.rkt")
+         (all-from-out "include/tree.rkt")
+         (all-from-out "include/types.rkt"))
 
