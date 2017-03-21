eaf34ba4d6719a95160938ff44fc0f008dfe1899
Bradley Busching <bradley.busching@gmail.com>
2017-03-21 11:52:13 -0700

    Allow creation of empty strarrays

--- libgit2/include/strarray.rkt
+++ libgit2/include/strarray.rkt
@@ -7,8 +7,9 @@
 
 ; Types
 
+(define _char-ptrptr (_cpointer/null _string))
 (define-cstruct _git_strarray
-  ([strings (_cpointer _string)]
+  ([strings _char-ptrptr]
    [count _size]))
 (define _strarray _git_strarray-pointer)
 (define _strarray/null _git_strarray-pointer/null)
@@ -20,3 +21,12 @@
 
 (define-libgit2/check git_strarray_copy
   (_fun _strarray _strarray -> _int))
+
+; misc
+
+(define make-strarray
+    (λ strs
+      (let [(strs-ptr (malloc _string (length strs)))]
+        (for ([i (range (length strs))])
+          (ptr-set! strs-ptr _string i (list-ref strs i)))
+        (make-git_strarray (cast strs-ptr _pointer _char-ptrptr) (length strs)))))
